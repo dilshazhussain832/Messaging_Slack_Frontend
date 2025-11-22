6e1f540eb397b4cd01f068e908b4d596d079ec91
Dilshaz_hussain832 <dilshazhussain123@gmail.com>
2025-11-22 15:13:49 +0530

    shadcn added

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,7 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
+import simpleImportSort from 'eslint-plugin-simple-import-sort'
 
 export default defineConfig([
   globalIgnores(['dist']),
@@ -13,6 +14,9 @@ export default defineConfig([
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
     ],
+    plugins: {
+      'simple-import-sort': simpleImportSort,
+    },
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
@@ -22,8 +26,14 @@ export default defineConfig([
         sourceType: 'module',
       },
     },
+
     rules: {
+      "semi": ["error", "always"],
+      "quotes": ["error", "single"],
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      
     },
   },
 ])
